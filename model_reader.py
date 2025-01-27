36f1938868d554869044ab70d1ac33415d3f439e
bdanzi <ary.d.anzi@gmail.com>
2025-01-27 14:57:32 +0100

    nonRes variables and no sample column in v2s

--- model_reader.py
+++ model_reader.py
@@ -12,8 +12,8 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import pyarrow.parquet as pq
-import matplotlib.pyplot as plt
-import mplhep as hep
+#import matplotlib.pyplot as plt
+#import mplhep as hep
 
 def load_yaml(file_path):
     with open(file_path, "r") as file:
@@ -33,9 +33,11 @@ if not os.path.isdir(model_dir):
     raise FileNotFoundError(f"Model directory '{model_dir}' does not exist.")
 
 # Search for YAML and .keras files in the model directory
-yaml_files = [f for f in os.listdir(model_dir) if f.endswith(".yaml")]
+parent_dir = os.path.dirname(model_dir)
+print(parent_dir)
+yaml_files = [f for f in os.listdir(parent_dir) if f.endswith(".yaml") and 'variables_train' in f]
 mod_files = [f for f in os.listdir(model_dir) if f.endswith(".keras")]
-
+print(yaml_files)
 if not yaml_files or len(yaml_files) > 1:
     raise FileNotFoundError("No YAML file found or too many YAML files in the model directory.")
 if not mod_files or len(mod_files) > 1:
@@ -46,7 +48,7 @@ model_path = os.path.join(model_dir, mod_files[0])
 print(f"Loading model from: {model_path}")
 model = tf.keras.models.load_model(model_path)
 
-variables_file = os.path.join(model_dir, yaml_files[0])
+variables_file = os.path.join(parent_dir, yaml_files[0])
 variables = load_yaml(variables_file)
 print(f"Loaded variables from: {variables_file}")
 
@@ -69,25 +71,13 @@ for var in input_variables:
                 - df_incl["dijet_mass"]
                 + 2 * 124.9
             )
-        elif var == "CosThetaStar_CS":
-            df_incl["CosThetaStar_CS"] = np.abs(df_incl["CosThetaStar_CS"])
-        elif var == "CosThetaStar_gg":
-            df_incl["CosThetaStar_gg"] = np.abs(df_incl["CosThetaStar_gg"])
-        elif var == "CosThetaStar_jj":
-            df_incl["CosThetaStar_jj"] = np.abs(df_incl["CosThetaStar_jj"])
-        elif var == "sublead_eta":
-            df_incl["sublead_eta"] = np.abs(df_incl["sublead_eta"])
-        elif var == "lead_bjet_eta":
-            df_incl["lead_bjet_eta"] = np.abs(df_incl["lead_bjet_eta"])
-        elif var == "sublead_bjet_eta":
-            df_incl["sublead_bjet_eta"] = np.abs(df_incl["sublead_bjet_eta"])
         elif var == "gg_pT_OverHHcand_mass":
             df_incl["gg_pT_OverHHcand_mass"] = (
-                df_incl["pt"] / df_incl["HHbbggCandidate_mass"]
+                df_incl["pt"] / df_incl["nonRes_HHbbggCandidate_mass"]
             )
         elif var == "jj_pT_OverHHcand_mass":
             df_incl["jj_pT_OverHHcand_mass"] = (
-                df_incl["dijet_pt"] / df_incl["HHbbggCandidate_mass"]
+                df_incl["nonRes_dijet_pt"] / df_incl["nonRes_HHbbggCandidate_mass"]
             )
         elif var == "lead_g_pT_OverHggcand_mass":
             df_incl["lead_g_pT_OverHggcand_mass"] = (
@@ -95,7 +85,7 @@ for var in input_variables:
             )
         elif var == "lead_j_pT_OverHbbcand_mass":
             df_incl["lead_j_pT_OverHbbcand_mass"] = (
-                df_incl["lead_bjet_pt"] / df_incl["dijet_mass"]
+                df_incl["nonRes_lead_bjet_pt"] / df_incl["nonRes_dijet_mass"]
             )
         elif var == "sublead_g_pT_OverHggcand_mass":
             df_incl["sublead_g_pT_OverHggcand_mass"] = (
@@ -103,8 +93,10 @@ for var in input_variables:
             )
         elif var == "sublead_j_pT_OverHbbcand_mass":
             df_incl["sublead_j_pT_OverHbbcand_mass"] = (
-                df_incl["sublead_bjet_pt"] / df_incl["dijet_mass"]
+                df_incl["nonRes_sublead_bjet_pt"] / df_incl["nonRes_dijet_mass"]
             )
+        elif f"nonRes_{var}" in df_incl.columns:
+            df_incl[var] = np.abs(df_incl[f"nonRes_{var}"])
         else:
             raise ValueError(f"Missing variable '{var}' is not recognized or cannot be computed.")
         
@@ -123,9 +115,9 @@ if args.pq_file_output:
     print(f"Output file saved as: {args.pq_file_output}.parquet")
 
 # Visualization
-list_samples = df_incl["sample"].unique()
-nodata = [sample for sample in list_samples if "Data" not in sample]
-
+#list_samples = df_incl["sample"].unique()
+#nodata = [sample for sample in list_samples if "Data" not in sample]
+"""
 fig, ax = plt.subplots(figsize=(10, 10))
 hep.style.use("CMS")
 hep.cms.label("Preliminary", data=True, lumi=22, year=2022)
@@ -144,3 +136,4 @@ for sample in nodata:
 
 plt.legend()
 plt.show()
+"""
